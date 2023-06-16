e0a65b4adc155e97f47787275e34df97216171e9
Luis Fernando <luisfborges2002@gmail.com>
2023-06-15 23:58:31 -0300

    Initial commit

---
+++ hrworker/src/test/java/com/ati/hrworker/HrworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ati.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
