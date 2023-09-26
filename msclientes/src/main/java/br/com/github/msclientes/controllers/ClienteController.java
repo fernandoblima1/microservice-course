482e5ef159b7838b72befbd89ae95bc5a3d7a25f
Luis Fernando Borges de Lima <luis.fernando@etipi.pi.gov.br>
2023-09-26 13:55:04 -0300

    :construction: Serviço de Gateway criado

--- msclientes/src/main/java/br/com/github/msclientes/controllers/ClienteController.java
+++ msclientes/src/main/java/br/com/github/msclientes/controllers/ClienteController.java
@@ -2,9 +2,8 @@ package br.com.github.msclientes.controllers;
 
 import java.net.URI;
 
-import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties.Web.Client;
-import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +13,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.github.msclientes.dtos.ClienteDTO;
 import br.com.github.msclientes.entities.Cliente;
 import br.com.github.msclientes.services.ClienteService;
-import jakarta.ws.rs.GET;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
@@ -28,9 +29,10 @@ public class ClienteController {
   private final ClienteService clienteService;
 
   @PostMapping()
-  public ResponseEntity<Object> criarCliente(@RequestBody ClienteDTO clienteDto) {
+  public ResponseEntity<Object> criarCliente(@RequestBody @Validated ClienteDTO clienteDto) {
     var cliente = clienteDto.toModel();
-    clienteService.salvar(cliente);
+    Cliente clienteSalvo = clienteService.salvar(cliente);
+    log.info("Criando cliente: {}", clienteSalvo);
     URI headerLocation = ServletUriComponentsBuilder.fromCurrentRequest().query("cpf={cpf}").buildAndExpand(cliente.getCpf()).toUri();
     return ResponseEntity.created(headerLocation).build();
   }
--- msclientes/src/main/java/br/com/github/msclientes/dtos/ClienteDTO.java
+++ msclientes/src/main/java/br/com/github/msclientes/dtos/ClienteDTO.java
@@ -1,12 +1,16 @@
 package br.com.github.msclientes.dtos;
 
 import br.com.github.msclientes.entities.Cliente;
+import io.micrometer.common.lang.NonNull;
 import lombok.Data;
 
 @Data
 public class ClienteDTO {
+  @NonNull
   private String nome;
+  @NonNull
   private String cpf;
+  @NonNull
   private Integer idade;
 
   public Cliente toModel() {
